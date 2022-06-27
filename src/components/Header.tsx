90e158c4b6cbf6164a9691ef47ca198271b8d9d7
cassio rodrigues <curriculocassio@hotmail.com>
2022-06-27 01:45:59 -0300

    adicionado menu hamburguer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
 import eduConectionIcon from '../assets/educonnect-icon.png'
 import cspLogoIcon from '../assets/csp-logo.png'
 import angloIcon from '../assets/anglo-logo.png'
+import { List, X } from 'phosphor-react'
+import { useState } from 'react'
 export function Header() {
   const classNavItem = 'text-blue-900 font-semibold py-3 h-full border-transparent hover:border-blue-900 border-b-4 transition-colors cursor-pointer'
+  const classNavItemHamburguer = 'text-white font-bold'
+  const [navbarOpen, setNavbarOpen] = useState(false)
   return (
     <header className="block">
       <div className="flex flex-wrap gap-4 items-center justify-between py-2">
@@ -25,7 +29,7 @@ export function Header() {
         <span className="text-blue-900 text-xl">Um lugar onde nascem os sonhos!</span>
       </div>
       <div className="block border-b">
-        <nav className="flex items-center justify-between h-full">
+        <nav className="flex items-center justify-between h-full lg:hidden">
           <span className={classNavItem}>Sobre</span>
           <span className={classNavItem}>Educação Básica</span>
           <span className={classNavItem}>Cursos Técnicos</span>
@@ -36,6 +40,31 @@ export function Header() {
           <span className={classNavItem}>Lista de Materiais</span>
           <span className={classNavItem}>LGPD</span>
         </nav>
+        {navbarOpen ? (
+          <div className='bg-blue-900 fixed -top-full left-0 w-screen h-screen z-[80] p-8 translate-y-full transition-transform'>
+            <span onClick={() => setNavbarOpen(false)}>
+              <X size={32} color="#fff" />
+            </span>
+            <div className='w-full h-full'>
+              <nav className="flex flex-col items-center justify-center gap-5 h-full">
+                <span className={classNavItemHamburguer}>Sobre</span>
+                <span className={classNavItemHamburguer}>Educação Básica</span>
+                <span className={classNavItemHamburguer}>Cursos Técnicos</span>
+                <span className={classNavItemHamburguer}>Agenda</span>
+                <span className={classNavItemHamburguer}>Notícias</span>
+                <span className={classNavItemHamburguer}>Plurall</span>
+                <span className={classNavItemHamburguer}>Portal Acadêmico</span>
+                <span className={classNavItemHamburguer}>Lista de Materiais</span>
+                <span className={classNavItemHamburguer}>LGPD</span>
+              </nav>
+            </div>
+          </div>
+        ) : (
+          <div className="flex-col items-center justify-center lg:flex hidden transition-transform" onClick={() => setNavbarOpen(true)}>
+            <span className='text-xs text-gray-600'>Menu</span>
+            <List size={40} className="text-blue-900" />
+          </div>
+        )}
       </div>
     </header>
   )
